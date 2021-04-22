b5c1ee0bdbc0fb000c66ef3ed821690be0f82c9e
Christopher Weller <Christopher.Weller@lcecorp.com>
2021-04-22 12:37:37 -0400

    UTF-8

--- KeepAlive.py
+++ KeepAlive.py
@@ -9,9 +9,9 @@ minutes = float(input("How many minutes do you want to chill for? "))
 moves = minutes*30
 for i in range(int(moves)):
 
-pyautogui.moveTo(firstposition, duration = 1)
-pyautogui.moveTo(secondposition, duration = 1)
-if i %10 == 0:
-pyautogui.moveTo(thirdposition, duration = 1)
-pyautogui.click()
-pyautogui.press]('space')
+	pyautogui.moveTo(firstposition, duration = 1)
+	pyautogui.moveTo(secondposition, duration = 1)
+	if i %10 == 0:
+		pyautogui.moveTo(thirdposition, duration = 1)
+		pyautogui.click()
+		pyautogui.press('space')
